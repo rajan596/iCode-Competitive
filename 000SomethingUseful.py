40f36388d273a8ad415289d6c899c68bc8d66fa2
rtkasodariya <kasodariyarajan@gmail.com>
2016-04-06 17:40:27 +0530

    links

---
+++ 000SomethingUseful.py
@@ -0,0 +1,9 @@
+# Python is Just for fun :)
+
+def codeforces(ProblemList): 
+	# Disjoint Set Union 
+	list=[]
+	list.append(
+		"Disjoint Set union Problems ",
+		"http://codeforces.com/problemset/tags/dsu?order=BY_SOLVED_DESC")
+
