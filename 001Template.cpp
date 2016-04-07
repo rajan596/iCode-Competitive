9b4192e0fbb8aaf121c23c7fcad9ea53fc3642b9
rtkasodariya <kasodariyarajan@gmail.com>
2016-04-07 16:51:02 +0530

    -

--- 001Template.cpp
+++ 001Template.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
+#define fin freopen("i1.txt","r",stdin)
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0);
 #define ll long long
 
 int main(){
-    //freopen("i1.txt","r",stdin);
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
 
 
 
